89bcc2bef3f2584368e90ba1384ef48f277acd86
akollgaa <kollgaarda@gmail.com>
2024-08-28 19:50:04 -0400

    Added temperature feature

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template
 from transport import Transport
+from temperature import Temperature
 
 app = Flask(__name__)
 
 bus = Transport()
 
+temp = Temperature()
+
 @app.route('/')
 def index():
     pred = bus.get_prediction(7097) # 7097 is the STOP id at forbes-wightman
+    tmp = temp.read_temp()
     return render_template('index.html', 
                            date=pred['date'], 
                            hour=pred['hour'], 
                            minute=pred['minute'],
                            second=pred['second'],
                            meridiem=pred['meridiem'],
-                           bus=pred['bus']
+                           bus=pred['bus'],
+                           temp=tmp
     )
