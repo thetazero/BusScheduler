929e1fecd5f7eb1f6a0ffd1ab5ac29d0a7e2357f
Aleksei Seletskiy <lesha.seletskiy@gmail.com>
2024-09-05 15:55:48 -0400

    delete

--- ts-rewrite/prt-client/src/App.tsx
+++ ts-rewrite/prt-client/src/App.tsx
@@ -1,7 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { BusArivalTime } from './types'
 import ArivalSlot from './ArivalSlots'
 
