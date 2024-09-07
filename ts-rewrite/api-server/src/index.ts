05fd335690996e3d8a336bed472fe8466cd19823
Aleksei Seletskiy <lesha.seletskiy@gmail.com>
2024-09-07 09:21:17 -0400

    10 second cache

--- ts-rewrite/api-server/src/index.ts
+++ ts-rewrite/api-server/src/index.ts
@@ -1,16 +1,20 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import apicache from 'apicache';
 
 import { extract_nice_predictions, get_prediction } from './prt';
 import { House, getHouse, getHouseStops } from './house';
 
 const app = express();
+const cache = apicache.middleware;
 
 app.use(cors());
 
 const port = process.env.PORT || 3000;
 
-app.get('/house/:house', async (req: Request, res: Response) => {
+app.get('/house/:house', 
+    cache('10 seconds'),
+    async (req: Request, res: Response) => {
     const house: House = getHouse(req.params.house);
     const stop_ids = getHouseStops(house);
     let result = await get_prediction(stop_ids)
